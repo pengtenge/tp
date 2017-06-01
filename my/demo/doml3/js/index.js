ab572b512ff623ab9f986e5e7a49f1572c6e274a
tengpenge <197512561@qq.com>
2017-06-01 11:53:59 +0800

    近期作品跟新
    2017.6.1

---
+++ my/demo/doml3/js/index.js
@@ -0,0 +1,21 @@
+$(function(){
+	$(".xuanxiang").click(function(){
+	$(".xf").show().animate({"left":0},300);
+	
+	})
+	
+		$(".xfr").click(function(){
+		console.log("1");
+		$(".xf").show().animate({"left":-1000},300);
+		})
+//      for (var i=0;i<$(".xfl").length;i++) {
+//		$(".xf").show().animate({"left":-180},300);
+//      	
+//      	
+//      }
+		
+
+	
+	
+	
+});
